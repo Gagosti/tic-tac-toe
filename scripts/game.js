9349196b07d673258060c391602a444e761c1425
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-11 20:25:17 -0300

    funcionalidade básica jogo -ok

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,16 @@ let board = ['','','','','','','','','']
 let playerTime = 0;
 let symbols = ['o','x']
 let gameOver = false;
+let winStates = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 function handleMove(position) {
 
@@ -27,19 +37,7 @@ function handleMove(position) {
 }
 
 function isWin() {
-
-  let winStates = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-
-  for(let i=0; i<winStates.length; i++) {
+   for(let i=0; i<winStates.length; i++) {
     let seq = winStates[i];
 
     let pos1 = seq[0];
@@ -52,7 +50,6 @@ function isWin() {
 
           return true
         }
-
   }
   return false;
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -12,7 +12,10 @@ function handleClick(event) {
   let square = event.target;
   let position = square.id;
 
-  handleMove(position);
+  if(handleMove(position)) {
+
+    setTimeout(() => {alert(`O jogo acabou - O Vencedor foi o jogoador ${playerTime}`);}, 10);
+  };
   updateSquares();
 }
 
@@ -28,3 +31,16 @@ function updateSquares() {
     }
   })
 }
+
+// function resetGame() {
+//   let squares = document.querySelectorAll(".square");
+//   let button = document.getElementById("reset");
+
+
+//   button.addEventListener("click", resetALL)
+// }
+
+// function resetALL(event) {
+//   let squares = document.querySelectorAll(".square");
+//   event.innerHTML = `<div></div>`
+// }
